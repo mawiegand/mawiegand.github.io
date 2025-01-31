9a802a1795d43f75d3201a2fefd4f7c3f8cc7b89
Marcel Wiegand <marcel@bluepanel.org>
2025-01-31 23:50:51 +0100

    ops: add CNAME file to `STATIC_PATHS`

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,12 @@
-AUTHOR = 'Marcel Wiegand'
-SITENAME = 'mawiegand/journal'
+AUTHOR = "Marcel Wiegand"
+SITENAME = "mawiegand/journal"
 SITEURL = ""
 
 PATH = "content"
 
-TIMEZONE = 'Europe/Berlin'
+TIMEZONE = "Europe/Berlin"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -33,3 +33,4 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
